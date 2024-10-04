0995a9905a8bc0e121ed127735f53a9b9a5b868b
Arely Muñoz <arelysinaimunoz@gmail.com>
2024-10-03 19:36:41 -0600

    anterior Nodo object added to Nodo.java class

--- src/listas/Nodo.java
+++ src/listas/Nodo.java
@@ -5,9 +5,11 @@ import colas.Trabajo;
 public class Nodo {
     public Trabajo dato;
     public Nodo siguiente;
+    public Nodo anterior;
 
     public Nodo(Trabajo dato) {
         this.dato = dato;
         this.siguiente = null;
+        this.anterior = null;
     }
 }
